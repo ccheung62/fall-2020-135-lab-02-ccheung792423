8a65db9c615c22847bec565ad1b4aba61eda7aa1
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-09-05 13:12:59 -0400

    finished it

---
+++ vaild.cpp
@@ -0,0 +1,33 @@
+/*
+Author: Cheuk Lam Cheung
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Input validation, Lab2A
+Sample Input/ Output: 
+$ ./valid
+Please enter an integer: -10
+Please re-enter: 1200
+Please re-enter: 100
+Please re-enter: 7
+
+Number squared is 49
+
+Write a program, which asks the user to input an integer in the range 0 < n < 100. If the number is out of range, the program should keep asking to re-enter until a valid number is input.
+
+After a valid value is obtained, print this number n squared.
+*/
+
+#include <iostream>
+
+int main () {
+  int num;
+  std::cout << "Please enter an integer: ";
+  std::cin >> num;
+  while (num <=0 || num>=100) {
+    std::cout << "Please re-enter: ";
+    std::cin >> num;
+  }
+  std::cout << "\nNumber squared is " << num*num << "\n";
+  return 0;
+  
+}
