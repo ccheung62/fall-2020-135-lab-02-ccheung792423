39a7c5b86fda88a0ba281b0ab4bb07fbc023c7b0
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-09-05 13:36:23 -0400

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+Author: Cheuk Lam Cheung
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Print all integers from the requested interval, Lab2B
+Sample Input/Output: 
+$ ./print-interval
+
+-5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9
+
+Write a program that asks the user to input two integers L and U (representing the lower and upper limits of the interval), and print out all integers in the range L ≤ i < U separated by spaces. Notice that we include the lower limit, but exclude the upper limit.
+*/
+
+#include <iostream>
+#include "funcs.h"
+
+int main () {
+  print_interval(-5, 10);
+  return 0;
+}
