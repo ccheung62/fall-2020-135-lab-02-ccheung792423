f4211510e142483caefb80caca4071704d8ebd08
Cheuk Lam Cheung <ccheung792423@gmail.com>
2020-09-05 14:37:37 -0400

    added comments

--- funcs.cpp
+++ funcs.cpp
@@ -2,8 +2,8 @@
 # include <iostream>
 
 void print_interval(int L, int U) {
-  for (int i = L; i<U; i++) {
-    std:: cout << i << " ";
+  for (int i = L; i<U; i++) {  
+    std:: cout << i << " ";  //prints and increase i until it reaches U
   }
   std::cout << "\n";
 }
